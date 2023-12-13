5d318e8be1e6492d4e3f3fb5335b7f5f495ea36f
i Komang Candra Suardinata <110315483+Manuelwell@users.noreply.github.com>
2023-12-13 09:23:10 +0700

    I Komang Candra Suardinata - Pong
    Base Gameplay

---
+++ Assets/script/BallControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+
+    // Update is called once per frame
+}
